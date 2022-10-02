aeab2417555b98f5aa3f676670005a9bb7595511
zjl <2814293468@qq.com>
2022-10-02 14:44:38 +0800

    bin

--- loader.asm
+++ loader.asm
@@ -3,7 +3,10 @@ org	0400h
 	mov	ax, 0B800h
 	mov	gs, ax
 	mov	ah, 0Fh				; 0000: 黑底    1111: 白字
-	mov	al, 'L'
+	mov	al, str
 	mov	[gs:((80 * 0 + 39) * 2)], ax	; 屏幕第 0 行, 第 39 列
 
 	jmp	$				; 到此停住
+
+
+str:    db 'This is zhangjianlongs boot'
